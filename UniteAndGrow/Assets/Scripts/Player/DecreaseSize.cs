e7e19752adac6416e1877752ab58d99e93a0cf87
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2019-01-19 14:33:23 -0500

    added increase and derease scripts

---
+++ UniteAndGrow/Assets/Scripts/Player/DecreaseSize.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecreaseSize : MonoBehaviour
+{
+    public float decreaseFactor_X = 0.1f;
+    public float decreaseFactor_Y = 0.1f;
+    public float decreaseFactor_Z = 0.1f;
+    public string decreaseObjectsTag;
+    public bool decreaseSizeOnMovement;
+
+    private Rigidbody objectRigidbody;
+
+    private void Start()
+    {
+        objectRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == decreaseObjectsTag)
+        {
+            if (decreaseSizeOnMovement)
+            {
+                // Only decrease size if moving on an object
+                if (objectRigidbody.velocity.magnitude != 0)
+                {
+                    transform.localScale = transform.localScale - (new Vector3(decreaseFactor_X / 100.0f, decreaseFactor_Y / 100.0f, decreaseFactor_Z / 100.0f));
+                }
+            }
+            else
+            {
+                // Always decrease size when touching an object
+                transform.localScale = transform.localScale - (new Vector3(decreaseFactor_X / 100.0f, decreaseFactor_Y / 100.0f, decreaseFactor_Z / 100.0f));
+
+            }
+
+        }
+    }
+}
---
+++ UniteAndGrow/Assets/Scripts/Player/IncreaseSize.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseSize : MonoBehaviour
+{
+    public float increaseFactor_X = 0.1f;
+    public float increaseFactor_Y = 0.1f;
+    public float increaseFactor_Z = 0.1f;
+    public string increaseObjectsTag;
+    public bool increaseSizeOnMovement;
+
+    private Rigidbody objectRigidbody;
+
+    private void Start()
+    {
+        objectRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag == increaseObjectsTag)
+        {
+            if(increaseSizeOnMovement)
+            {
+                // Only increase size if moving on an object
+                if(objectRigidbody.velocity.magnitude != 0)
+                {
+                    transform.localScale = transform.localScale + (new Vector3(increaseFactor_X / 100.0f, increaseFactor_Y / 100.0f, increaseFactor_Z / 100.0f));
+                }
+            }
+            else
+            {
+                // Always increase size when touching an object
+                transform.localScale = transform.localScale + (new Vector3(increaseFactor_X / 100.0f, increaseFactor_Y / 100.0f, increaseFactor_Z / 100.0f));
+
+            }
+
+        }
+    }
+
+    
+}
