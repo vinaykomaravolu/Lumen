901c794d9bca05094adbe83b4c62f53d3841c220
Zhenyao She <dr.tobyshe@gmail.com>
2019-01-19 15:03:48 -0500

    player control save in progress

--- UniteAndGrow/Assets/Scripts/Player/PlayerControl.cs
+++ UniteAndGrow/Assets/Scripts/Player/PlayerControl.cs
@@ -2,17 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
-{
+public class PlayerControl : MonoBehaviour{
+
+    public GameObject playerCamera;
+    public float speed;
+    public float jumpSpeed;
+    private Rigidbody body;
+
+    public float maxJumpCount;
+    private float jumpCount = 0;
+    private Vector3 contactNorm;
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+        Vector3 velocity = body.velocity;
+        Vector3 controlStick = new Vector3(Input.GetAxis("Horizontal"), 
+                                   0, Input.GetAxis("Vertical")) * speed;
         
+        //movement
+        velocity.x =setSpeed(Input.GetAxis("Horizontal") * speed, velocity.x);
+        velocity.z = setSpeed(Input.GetAxis("Vertical") * speed, velocity.z);
+        
+        //jump
+        if (canJump()){
+            velocity.y = 0;
+            if (contactNorm.y > 0.7f)
+            velocity.y = jumpSpeed;
+            jumpCount--;
+        }
+
+        body.velocity = velocity;
+    }
+
+    private float setSpeed(float control, float current){
+        if (control * current < 0){
+            // control is trying to move to the opposite direction
+            return current + control;
+        }
+
+        if (Mathf.Abs(control) < Mathf.Abs(current)){
+            return current;
+        }
+
+        return control;
+    }
+
+    private bool canJump(){
+        return Input.GetButtonDown("Jump") && jumpCount > 0;
+    }
+
+    private void OnCollisionStay(Collision other){
+        checkWallSlope(other);
+    }
+
+    private void checkWallSlope(Collision other){
+        contactNorm = new Vector3(0, -1, 0);
+        
+        //looking for the flattest contact surface
+        for (int i = 0; i < other.contactCount; i++){
+            Vector3 norm = other.GetContact(i).normal;
+            if (norm.y > contactNorm.y) contactNorm = norm;
+        }
+        
+        //reset jump count when contact
+        if (contactNorm.y >= 0) jumpCount = maxJumpCount;
     }
 }
