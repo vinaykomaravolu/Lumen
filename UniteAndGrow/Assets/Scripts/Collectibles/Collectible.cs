4c4b02d0fff329e42d846da8f8dc5afac94472fb
Vinay Komaravolu <vinay.komaravolu@mail.utoronto.ca>
2019-01-21 19:40:49 -0500

    added new scripts

---
+++ UniteAndGrow/Assets/Scripts/Collectibles/Collectible.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectible : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 30, 0) * 5 * Time.deltaTime);
+    }
+}
