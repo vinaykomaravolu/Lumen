b59e005ee7a8f0a9a853f80c47de829d3e484c07
kevinleung987 <kevin.leung987@hotmail.com>
2019-02-21 20:30:01 -0500

    Fix Death UI

--- UniteAndGrow/Assets/DeathUIFade.cs
+++ UniteAndGrow/Assets/DeathUIFade.cs
@@ -7,24 +7,12 @@ public class DeathUIFade : MonoBehaviour
     public Text uiText;
     public Image backgroundImage;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            StartCoroutine(FadeTextToFullAlpha(1f));
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            uiText.color = new Color(uiText.color.r, uiText.color.g, uiText.color.b, 0);
-            backgroundImage.color = new Color(backgroundImage.color.r, backgroundImage.color.g, backgroundImage.color.b, 0);
-        }
-    }
-
     public IEnumerator FadeTextToFullAlpha(float t)
     {
         Text j = this.uiText;
         Image i = this.backgroundImage;
         i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        j.color = new Color(j.color.r, j.color.g, j.color.b, 0);
         while (i.color.a < 0.85f)
         {
             i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
