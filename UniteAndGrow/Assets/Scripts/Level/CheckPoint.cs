09760db2a476d5bd4b0d4c9e0f82992cd839e364
Zhenyao She <dr.tobyshe@gmail.com>
2019-03-13 21:29:33 -0400

    added checkpoint in level 1

--- UniteAndGrow/Assets/Scripts/Level/CheckPoint.cs
+++ UniteAndGrow/Assets/Scripts/Level/CheckPoint.cs
@@ -1,9 +1,11 @@
+using Objects;
 using UnityEngine;
 
 class CheckPoint : MonoBehaviour{
 
     public int checkPointIndex;
     public GameObject spawnPoint;
+    public CheckPointMarker[] markers;
         
     private void OnTriggerEnter(Collider other){
         if (Global.gameControl.checkPointIndex < checkPointIndex){
