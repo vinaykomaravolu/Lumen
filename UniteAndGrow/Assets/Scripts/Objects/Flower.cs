61e91ab66a592600c5efa011770a31c6942d2ad9
Ben <crbac3217@gmail.com>
2019-03-21 10:49:38 -0400

    assets
    added flowers and plants

---
+++ UniteAndGrow/Assets/Scripts/Objects/Flower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    Animator flower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flower = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            flower.SetBool("inter", true);
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            flower.SetBool("inter", false);
+        }
+    }
+}
---
+++ UniteAndGrow/Assets/Scripts/Objects/Plant2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant2 : MonoBehaviour
+{
+    Animator plant2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plant2 = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            plant2.SetTrigger("pass");
+        }
+    }
+}
