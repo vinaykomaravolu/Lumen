227a8ff29b960f523f2ef72ec8047029aaab94c0
vinaykomaravolu <vinay.komaravolu@mail.utoronto.ca>
2019-03-29 16:38:08 -0400

    changed collectible

---
+++ UniteAndGrow/Assets/Scripts/Player/RandomRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour { 
+
+    public float rotation_x = 10f;
+    public float rotation_y = 30f;
+    public float rotation_z = 20f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(1, 0, 0), rotation_x * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 1, 0), rotation_y * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, 1), rotation_z * Time.deltaTime);
+    }
+}
